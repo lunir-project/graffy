96e9694de06e3a57bf8cde37472d3feb175dd6ce
dyslexicsteak <49301588+dyslexicsteak@users.noreply.github.com>
2023-03-25 13:44:29 +0300

    Change test

--- src/graph/graph/tests.rs
+++ src/graph/graph/tests.rs
@@ -5,22 +5,23 @@ use crate::graph::dft::Dft;
 
 #[test]
 fn create_graph() {
-    let mut graph = Graph::<usize, usize, Directed, Cyclic>::new_directed();
+    let mut graph = Graph::<usize, usize, Directed, Acyclic>::new_directed();
 
-    let vertex2 = graph.insert_vertex(20);
-    let vertex3 = graph.insert_vertex(30);
-    let vertex1 = graph.insert_vertex(10);
-    let vertex4 = graph.insert_vertex(40);
+    let v1 = graph.insert_vertex(20);
+    let v2 = graph.insert_vertex(30);
+    let v3 = graph.insert_vertex(40);
 
-    graph.insert_edge(vertex2, vertex3, 25).unwrap();
-    graph.insert_edge(vertex3, vertex1, 50).unwrap();
-    graph.insert_edge(vertex1, vertex2, 75).unwrap();
+    graph.insert_edge(v1, v2, 25).unwrap();
+    graph.insert_edge(v2, v2, 25).unwrap();
+    graph.insert_edge(v3, v1, 75).unwrap();
 
     let traverser = Dft::from(&graph);
 
     for (index, vertex) in traverser.enumerate() {
-        println!("{index}: {:#?}", vertex);
+        eprintln!("{index}: {:#?}", vertex);
     }
+
+    panic!()
 
     // where do we check..
 }
