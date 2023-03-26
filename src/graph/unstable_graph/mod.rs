39dd7c3446ba452a37c0efeb3412222fe6ae6fed
dyslexicsteak <49301588+dyslexicsteak@users.noreply.github.com>
2023-03-26 16:41:29 +0300

    Add documentation, several small tweaks, missing methods

--- src/graph/unstable_graph/mod.rs
+++ src/graph/unstable_graph/mod.rs
@@ -3,23 +3,34 @@ mod tests;
 
 use super::*;
 use dft::*;
-
 use std::marker::PhantomData;
+
+/// Uninhabited type that indicates that an [`UnstableGraph`] is cyclic.
 #[derive(Clone, Debug)]
 pub enum Cyclic {}
+
+/// Uninhabited type that indicates that [`UnstableGraph`] is acyclic.
 #[derive(Clone, Debug)]
 pub enum Acyclic {}
 
+/// A trait for [`UnstableGraph`]'s cyclicness types.
 pub trait Cyclicness: Sized {
+    /// The edge insertion behaviour for cyclic or acyclic [`UnstableGraph]`s.
+    ///
+    /// # Fallible:
+    /// Returns an `Err` if the [`UnstableGraph`] being inserted to is acyclic and the insertion
+    /// of this [`Edge`] would introduce a cycle.
     fn insert_edge<V: Clone, E: Clone, D: EdgeType>(
         graph: &mut UnstableGraph<V, E, D, Self>,
         edge: Edge<E>,
     ) -> Result<usize, CycleError>;
 
+    /// Returns `true` if the type is cyclic.
     fn is_cyclic() -> bool;
 }
 
 impl Cyclicness for Cyclic {
+    /// The edge insertion behaviour for cyclic [`UnstableGraph`]s.
     fn insert_edge<V: Clone, E: Clone, D: EdgeType>(
         graph: &mut UnstableGraph<V, E, D, Self>,
         edge: Edge<E>,
@@ -27,12 +38,14 @@ impl Cyclicness for Cyclic {
         Ok(graph.insert_edge_unchecked(edge))
     }
 
+    /// Returns `true` if the type is cyclic. Always returns `true`.
     fn is_cyclic() -> bool {
         true
     }
 }
 
 impl Cyclicness for Acyclic {
+    /// The edge insertion behaviour for cyclic [`UnstableGraph`]s.
     fn insert_edge<V: Clone, E: Clone, D: EdgeType>(
         graph: &mut UnstableGraph<V, E, D, Self>,
         edge: Edge<E>,
@@ -43,75 +56,50 @@ impl Cyclicness for Acyclic {
         Ok(graph.insert_edge_unchecked(edge))
     }
 
+    /// Returns `true` if the type is cyclic. Always returns `false`.
     fn is_cyclic() -> bool {
         false
     }
 }
 
-
-
-pub enum Direction {
-    Outgoing,
-    Incoming,
-}
-
-impl Direction {
-    #[inline]
-    pub fn opposite(self) -> Self {
-        match self {
-            Self::Outgoing => Self::Incoming,
-            Self::Incoming => Self::Outgoing,
-        }
-    }
-}
-
+/// The vertex type for [`UnstableGraph`].
 #[derive(Clone, Debug)]
 pub struct Vertex<V: Clone> {
     pub weight: V,
-    pub edges: Vec<usize>,
+    edge_indices: Vec<usize>,
 }
 
+/// The edge type for [`UnstableGraph`].
 #[derive(Clone, Debug)]
 pub struct Edge<E: Clone> {
     pub weight: E,
-    pub vertices: [usize; 2],
+    vertex_indices: [usize; 2],
 }
 
 impl<V: Clone> Vertex<V> {
-    #[inline]
-    fn new(weight: V) -> Vertex<V> {
-        Vertex {
-            weight,
-            edges: vec![],
-        }
-    }
-
     fn associate_edge(&mut self, index: usize) {
-        match self.edges.iter().find(|&&idx| idx == index) {
+        match self.edge_indices.iter().find(|&&idx| idx == index) {
             Some(_) => return,
-            None => self.edges.push(index),
+            None => self.edge_indices.push(index),
         }
     }
 
     fn dissociate_edge(&mut self, index: usize) {
-        let v = self
-            .edges
-            .iter()
-            .filter(|&&idx| idx != index)
-            .cloned()
-            .collect();
-
-        self.edges = v;
+        self.edge_indices.retain(|&idx| idx != index);
     }
 
+    /// Gets the indices of the [`Edge`]s that are connected to this [`Vertex`].
     #[inline]
     pub fn edge_indices(&self) -> &[usize] {
-        &self.edges
+        &self.edge_indices
     }
 
     #[inline]
-    pub fn weight(&self) -> &V {
-        &self.weight
+    fn new(weight: V) -> Vertex<V> {
+        Vertex {
+            weight,
+            edge_indices: vec![],
+        }
     }
 }
 
@@ -120,28 +108,37 @@ impl<E: Clone> Edge<E> {
     fn new(source: usize, destination: usize, weight: E) -> Edge<E> {
         Edge {
             weight,
-            vertices: [source, destination],
+            vertex_indices: [source, destination],
         }
     }
 
-    fn vertex_indices(&self) -> (usize, usize) {
-        (self.vertices[0], self.vertices[1])
+    /// Gets the indices of the [`Vertices`] that are connected to this [`Edge`].
+    pub fn vertex_indices(&self) -> (usize, usize) {
+        (self.vertex_indices[0], self.vertex_indices[1])
     }
 }
 
 #[derive(Clone, Debug)]
 pub struct UnstableGraph<V: Clone, E: Clone, D: EdgeType, C: Cyclicness> {
-    pub vertices: Vec<Vertex<V>>,
-    pub edges: Vec<Edge<E>>,
-    _directed: PhantomData<D>,
     _cyclic: PhantomData<C>,
+    _directed: PhantomData<D>,
+    pub edges: Vec<Edge<E>>,
+    pub vertices: Vec<Vertex<V>>,
 }
 
 impl<V: Clone, E: Clone, D: EdgeType, C: Cyclicness> UnstableGraph<V, E, D, C> {
+    /// Returns `true` if this [`UnstableGraph`] contains a cycle.
     pub fn cycles(&self) -> bool {
         Dft::from(self).cycles()
     }
 
+    /// Returns the number of edges in this [`UnstableGraph`].
+    #[inline]
+    pub fn edge_count(&self) -> usize {
+        self.edges.len()
+    }
+
+    /// Inserts an [`Edge`] into this [`UnstableGraph`].
     #[inline]
     pub fn insert_edge(
         &mut self,
@@ -177,28 +174,80 @@ impl<V: Clone, E: Clone, D: EdgeType, C: Cyclicness> UnstableGraph<V, E, D, C> {
         index
     }
 
+    /// Inserts a [`Vertex`] into this [`UnstableGraph`].
     #[inline]
     pub fn insert_vertex(&mut self, weight: V) -> usize {
-        self.vertices.push(Vertex {
-            weight,
-            edges: vec![],
-        });
+        self.vertices.push(Vertex::new(weight));
         self.vertices.len() - 1
     }
 
+    /// Removes the [`Edge`] at `index` from this [`UnstableGraph`] while associating edges with
+    /// and dissociating edges from [`Vertex`]es as necessary. This method incurs greater overhead
+    /// relative to its counterpart [`remove_edge_simple`][`Self::remove_edge_simple()`], but is needed
+    /// to prevent the loss of an [`UnstableGraph`]'s meaning as a result of modifying its shape.
     #[inline]
     pub fn remove_edge(&mut self, index: usize) {
+        let top_index = self.edges.len() - 1;
+        let top = &self.edges[top_index];
+
+        for idx in top.vertex_indices {
+            let vertex = &mut self.vertices[idx];
+            vertex.dissociate_edge(top_index);
+            vertex.associate_edge(index)
+        }
+
+        self.remove_edge_simple(index);
+    }
+
+    /// Removes the [`Edge`] at `index` from this [`UnstableGraph`]. Note that this is may
+    /// lead to unintended consequences as the [`Vertex`]es that are associated with the [`Edge`]
+    /// at `index` may become associated with  another [`Edge`] at the same index. To avoid
+    /// this, use [`remove_edge`][`Self::remove_edge()`].
+    #[inline]
+    pub fn remove_edge_simple(&mut self, index: usize) {
         self.edges.swap_remove(index);
     }
 
+    /// Removes the [`Vertex`] at `index` from this [`UnstableGraph`] while reconfiguring and
+    /// removing [`Edge`]s as necessary. Any edges that were connected to the [`Vertex`] at `index`
+    /// will be removed.  This method incurs considerable overhead relative to its counterpart
+    /// [`remove_vertex_simple`][`Self::remove_vertex_simple()`], but is needed to prevent the
+    /// loss of an [`UnstableGraph`]'s meaning as a result of modifying its shape.
+    #[inline]
+    pub fn remove_vertex(&mut self, index: usize) {
+        let top_index = self.vertices.len() - 1;
+        let top = self.vertices[top_index].clone();
+        let target = self.vertices[index].clone();
+
+        for idx in target.edge_indices {
+            self.remove_edge(idx);
+        }
+
+        for idx in top.edge_indices {
+            self.edges[idx].vertex_indices = self.edges[idx]
+                .vertex_indices
+                .iter()
+                .map(|&ix| if ix == top_index { index } else { ix })
+                .collect::<Vec<_>>()
+                .try_into()
+                .unwrap()
+        }
+
+        self.remove_edge_simple(index);
+    }
+
+    /// Removes the [`Vertex`] at `index` from this [`UnstableGraph`]. Note that this is may lead to
+    /// unintended consequences as the [`Edge`]s that point to the [`Vertex`] at `index`
+    /// may point to another [`Vertex`] at the same index. To avoid this, use [`remove_vertex`][`Self::remove_vertex()`].
+    #[inline]
+    pub fn remove_vertex_simple(&mut self, index: usize) {
+        self.vertices.swap_remove(index);
+    }
+
+    /// Returns the number of vertices in this [`UnstableGraph`].
     #[inline]
     pub fn vertex_count(&self) -> usize {
         self.vertices.len()
-    }
-
-    #[inline]
-    pub fn edge_count(&self) -> usize {
-        self.edges.len()
     }
 
     fn would_cycle_with_edge(&mut self, edge: Edge<E>) -> bool {
@@ -212,8 +261,21 @@ impl<V: Clone, E: Clone, D: EdgeType, C: Cyclicness> UnstableGraph<V, E, D, C> {
 }
 
 impl<V: Clone, E: Clone> UnstableGraph<V, E, Directed, Cyclic> {
+    /// Creates a new directed cyclic [`UnstableGraph`].
     #[inline]
-    pub fn new_directed_with_capacity(cap: usize) -> Self {
+    pub fn directed() -> Self {
+        UnstableGraph {
+            vertices: vec![],
+            edges: vec![],
+            _directed: PhantomData,
+            _cyclic: PhantomData,
+        }
+    }
+
+    /// Creates a new directed cyclic [`UnstableGraph`] with a preallocated
+    /// capacity for `cap` vertices and `cap` edges.
+    #[inline]
+    pub fn directed_with_capacity(cap: usize) -> Self {
         UnstableGraph {
             vertices: Vec::with_capacity(cap),
             edges: Vec::with_capacity(cap),
@@ -222,11 +284,17 @@ impl<V: Clone, E: Clone> UnstableGraph<V, E, Directed, Cyclic> {
         }
     }
 
-    #[inline]
-    pub fn new_directed() -> Self {
+    /// Creates a new directed cyclic [`UnstableGraph`] with a preallocated
+    /// capacity for `cap` vertices and approximately `cap` * `ratio` edges.
+    /// This method can be used to further optimise to reduce allocations when the
+    /// approximate ratio of vertices to edges is known in addition to the approximate
+    /// final number of vertices.
+    pub fn directed_with_capacity_and_factor(cap: usize, ratio: f32) -> Self {
+        let edge_cap = (cap as f32 * ratio) as usize;
+
         UnstableGraph {
-            vertices: vec![],
-            edges: vec![],
+            vertices: Vec::with_capacity(cap),
+            edges: Vec::with_capacity(edge_cap),
             _directed: PhantomData,
             _cyclic: PhantomData,
         }
@@ -234,8 +302,21 @@ impl<V: Clone, E: Clone> UnstableGraph<V, E, Directed, Cyclic> {
 }
 
 impl<V: Clone, E: Clone> UnstableGraph<V, E, Directed, Acyclic> {
+    /// Creates a new directed acyclic [`UnstableGraph`].
     #[inline]
-    pub fn new_directed_with_capacity(cap: usize) -> Self {
+    pub fn directed() -> Self {
+        UnstableGraph {
+            vertices: vec![],
+            edges: vec![],
+            _directed: PhantomData,
+            _cyclic: PhantomData,
+        }
+    }
+
+    /// Creates a new directed acyclic [`UnstableGraph`] with a preallocated
+    /// capacity for `cap` vertices and `cap` edges.
+    #[inline]
+    pub fn directed_with_capacity(cap: usize) -> Self {
         UnstableGraph {
             vertices: Vec::with_capacity(cap),
             edges: Vec::with_capacity(cap),
@@ -244,11 +325,17 @@ impl<V: Clone, E: Clone> UnstableGraph<V, E, Directed, Acyclic> {
         }
     }
 
-    #[inline]
-    pub fn new_directed() -> Self {
+    /// Creates a new directed acyclic [`UnstableGraph`] with a preallocated
+    /// capacity for `cap` vertices and approximately `cap` * `ratio` edges.
+    /// This method can be used to further optimise to reduce allocations when the
+    /// approximate ratio of vertices to edges is known in addition to the approximate
+    /// final number of vertices.
+    pub fn directed_with_capacity_and_factor(cap: usize, ratio: f32) -> Self {
+        let edge_cap = (cap as f32 * ratio) as usize;
+
         UnstableGraph {
-            vertices: vec![],
-            edges: vec![],
+            vertices: Vec::with_capacity(cap),
+            edges: Vec::with_capacity(edge_cap),
             _directed: PhantomData,
             _cyclic: PhantomData,
         }
@@ -256,8 +343,21 @@ impl<V: Clone, E: Clone> UnstableGraph<V, E, Directed, Acyclic> {
 }
 
 impl<V: Clone, E: Clone> UnstableGraph<V, E, Undirected, Cyclic> {
+    /// Creates a new undirected cyclic [`UnstableGraph`].
     #[inline]
-    pub fn new_undirected_with_capacity(cap: usize) -> Self {
+    pub fn undirected() -> Self {
+        UnstableGraph {
+            vertices: vec![],
+            edges: vec![],
+            _directed: PhantomData,
+            _cyclic: PhantomData,
+        }
+    }
+
+    /// Creates a new undirected cyclic [`UnstableGraph`] with a preallocated
+    /// capacity for `cap` vertices and `cap` edges.
+    #[inline]
+    pub fn undirected_with_capacity(cap: usize) -> Self {
         UnstableGraph {
             vertices: Vec::with_capacity(cap),
             edges: Vec::with_capacity(cap),
@@ -266,11 +366,17 @@ impl<V: Clone, E: Clone> UnstableGraph<V, E, Undirected, Cyclic> {
         }
     }
 
-    #[inline]
-    pub fn new_undirected() -> Self {
+    /// Creates a new undirected cyclic [`UnstableGraph`] with a preallocated
+    /// capacity for `cap` vertices and approximately `cap * ratio` edges.
+    /// This method can be used to further optimise to reduce allocations when the
+    /// approximate ratio of vertices to edges is known in addition to the approximate
+    /// final number of vertices.
+    pub fn undirected_with_capacity_and_factor(cap: usize, ratio: f32) -> Self {
+        let edge_cap = (cap as f32 * ratio) as usize;
+
         UnstableGraph {
-            vertices: vec![],
-            edges: vec![],
+            vertices: Vec::with_capacity(cap),
+            edges: Vec::with_capacity(edge_cap),
             _directed: PhantomData,
             _cyclic: PhantomData,
         }
