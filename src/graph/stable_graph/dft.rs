0057f9f29b7968a97090e7d22280ac48dbae20a2
dyslexicsteak <49301588+dyslexicsteak@users.noreply.github.com>
2023-03-26 10:50:29 +0300

    Add explicit lifetime

--- src/graph/stable_graph/dft.rs
+++ src/graph/stable_graph/dft.rs
@@ -1,15 +1,15 @@
 use super::{Acyclic, Cyclic, Cyclicness, Edge, EdgeType, StableGraph, Vertex};
 use std::{collections::HashSet, marker::PhantomData};
 
-pub struct Dft<V: Clone, E: Clone, C: Cyclicness> {
+pub struct Dft<'g, V: Clone + 'g, E: Clone, C: Cyclicness> {
     vertices: Vec<Vertex<V>>,
     visited_vertices: HashSet<usize>,
     edges: Vec<Edge<E>>,
     counter: usize,
-    _cyclic: PhantomData<C>,
+    _cyclic: PhantomData<&'g C>,
 }
 
-impl<V: Clone, E: Clone, C: Cyclicness> Dft<V, E, C> {
+impl<'g, V: Clone, E: Clone, C: Cyclicness> Dft<'g, V, E, C> {
     #[inline]
     pub fn cycles(&self) -> bool {
         todo!()
@@ -21,8 +21,8 @@ impl<V: Clone, E: Clone, C: Cyclicness> Dft<V, E, C> {
     }
 }
 
-impl<V: Clone, E: Clone, D: EdgeType, C: Cyclicness> From<&StableGraph<V, E, D, C>>
-    for Dft<V, E, C>
+impl<'g, V: Clone, E: Clone, D: EdgeType, C: Cyclicness> From<&StableGraph<V, E, D, C>>
+    for Dft<'g, V, E, C>
 {
     fn from(from: &StableGraph<V, E, D, C>) -> Self {
         Dft {
@@ -35,7 +35,7 @@ impl<V: Clone, E: Clone, D: EdgeType, C: Cyclicness> From<&StableGraph<V, E, D, 
     }
 }
 
-impl<V: Clone, E: Clone> Dft<V, E, Cyclic> {
+impl<'g, V: Clone, E: Clone> Dft<'g, V, E, Cyclic> {
     pub fn new_cyclic<D: EdgeType, C: Cyclicness>(from: &StableGraph<V, E, D, C>) -> Self {
         Dft {
             vertices: from.vertices.values().cloned().collect(),
@@ -47,7 +47,7 @@ impl<V: Clone, E: Clone> Dft<V, E, Cyclic> {
     }
 }
 
-impl<V: Clone, E: Clone> Dft<V, E, Acyclic> {
+impl<'g, V: Clone, E: Clone> Dft<'g, V, E, Acyclic> {
     pub fn new_acyclic<D: EdgeType, C: Cyclicness>(from: &StableGraph<V, E, D, C>) -> Self {
         Dft {
             vertices: from.vertices.values().cloned().collect(),
@@ -59,11 +59,10 @@ impl<V: Clone, E: Clone> Dft<V, E, Acyclic> {
     }
 }
 
-impl<V: Clone, E: Clone, C: Cyclicness> Iterator for Dft<V, E, C> {
-    type Item = Vertex<V>;
+impl<'g, V: Clone, E: Clone, C: Cyclicness> Iterator for Dft<'g, V, E, C> {
+    type Item = &'g Vertex<V>;
 
     fn next(&mut self) -> Option<Self::Item> {
         todo!()
     }
 }
-
