63861614f6c4db0016be0f93355cc85d843caa35
dyslexicsteak <49301588+dyslexicsteak@users.noreply.github.com>
2023-03-26 07:14:56 +0300

    Deduplicate types, rename graph to unstable_graph

--- src/graph/mod.rs
+++ src/graph/mod.rs
@@ -1,3 +1,26 @@
-mod dft;
-mod graph;
+mod unstable_graph;
 mod stable_graph;
+
+#[derive(Debug)]
+pub struct CycleError;
+
+#[derive(Clone, Debug)]
+pub enum Directed {}
+#[derive(Clone, Debug)]
+pub enum Undirected {}
+
+pub trait EdgeType {
+    fn is_directed(&self) -> bool;
+}
+
+impl EdgeType for Directed {
+    fn is_directed(&self) -> bool {
+        true
+    }
+}
+
+impl EdgeType for Undirected {
+    fn is_directed(&self) -> bool {
+        false
+    }
+}
