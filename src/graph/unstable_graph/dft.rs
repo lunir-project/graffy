536ac99a2fb7f83ba6792e6e407c27a586a5febd
dyslexicsteak <49301588+dyslexicsteak@users.noreply.github.com>
2023-03-26 16:41:03 +0300

    Add documentation, fix names to reflect changes to mod.rs

--- src/graph/unstable_graph/dft.rs
+++ src/graph/unstable_graph/dft.rs
@@ -1,6 +1,8 @@
 use super::unstable_graph::{Acyclic, Cyclic, Cyclicness, Edge, EdgeType, UnstableGraph, Vertex};
 use std::{collections::HashSet, marker::PhantomData};
 
+/// A depth-first traverser which yields items from an [`UnstableGraph`] on a
+/// [depth-first](https://en.wikipedia.org/wiki/Depth-first_search) basis.
 pub struct Dft<'g, V: Clone, E: Clone, C: Cyclicness> {
     vertices: &'g [Vertex<V>],
     visited_vertices: HashSet<usize>,
@@ -10,25 +12,27 @@ pub struct Dft<'g, V: Clone, E: Clone, C: Cyclicness> {
 }
 
 impl<'g, V: Clone, E: Clone, C: Cyclicness> Dft<'g, V, E, C> {
+    /// Returns if the [`UnstableGraph`] that this [`Dft`] is iterating over
+    /// contains a cycle.
     #[inline]
     pub fn cycles(&self) -> bool {
         let mut visited = HashSet::new();
         let mut pending_checks = vec![];
 
-        visited.insert(self.edges[0].vertices[0]);
-        pending_checks.push(self.edges[0].vertices[0]);
+        visited.insert(self.edges[0].vertex_indices[0]);
+        pending_checks.push(self.edges[0].vertex_indices[0]);
 
         while !pending_checks.is_empty() {
             let vertex = pending_checks.pop().unwrap();
 
-            for edge in &self.vertices[vertex].edges {
-                let adjacent = self.edges[*edge].vertices[1];
+            for edge in &self.vertices[vertex].edge_indices {
+                let adjacent = self.edges[*edge].vertex_indices[1];
                 if visited.insert(adjacent) {
                     pending_checks.push(adjacent);
                 } else if self.vertices[adjacent]
-                    .edges
+                    .edge_indices
                     .iter()
-                    .any(|&e| e != *edge && self.edges[e].vertices[1] == adjacent)
+                    .any(|&e| e != *edge && self.edges[e].vertex_indices[1] == adjacent)
                 {
                     return true;
                 }
@@ -38,6 +42,8 @@ impl<'g, V: Clone, E: Clone, C: Cyclicness> Dft<'g, V, E, C> {
         false
     }
 
+    /// Creates a new [`Dft`] from an [`UnstableGraph`]; the cyclicness of the resulting traverser
+    /// is the same as the cyclicness of the underlying graph.
     #[inline]
     pub fn new<D: EdgeType>(from: &'g UnstableGraph<V, E, D, C>) -> Self {
         Self::from(from)
@@ -47,6 +53,8 @@ impl<'g, V: Clone, E: Clone, C: Cyclicness> Dft<'g, V, E, C> {
 impl<'g, V: Clone, E: Clone, D: EdgeType, C: Cyclicness> From<&'g UnstableGraph<V, E, D, C>>
     for Dft<'g, V, E, C>
 {
+    /// Creates a new [`Dft`] from an [`UnstableGraph`]; the cyclicness of the resulting traverser
+    /// is the same as the cyclicness of the underlying graph.
     fn from(from: &'g UnstableGraph<V, E, D, C>) -> Self {
         Dft {
             vertices: &from.vertices.as_slice(),
@@ -59,6 +67,7 @@ impl<'g, V: Clone, E: Clone, D: EdgeType, C: Cyclicness> From<&'g UnstableGraph<
 }
 
 impl<'g, V: Clone, E: Clone> Dft<'g, V, E, Cyclic> {
+    /// Creates a new cyclic [`Dft`] from an [`UnstableGraph`].
     pub fn new_cyclic<D: EdgeType, C: Cyclicness>(from: &'g UnstableGraph<V, E, D, C>) -> Self {
         Dft {
             vertices: &from.vertices.as_slice(),
@@ -71,6 +80,7 @@ impl<'g, V: Clone, E: Clone> Dft<'g, V, E, Cyclic> {
 }
 
 impl<'g, V: Clone, E: Clone> Dft<'g, V, E, Acyclic> {
+    /// Creates a new [`Dft`] from an [`UnstableGraph`].
     pub fn new_acyclic<D: EdgeType, C: Cyclicness>(from: &'g UnstableGraph<V, E, D, C>) -> Self {
         Dft {
             vertices: &from.vertices.as_slice(),
@@ -82,24 +92,23 @@ impl<'g, V: Clone, E: Clone> Dft<'g, V, E, Acyclic> {
     }
 }
 
-// TODO: StableGraph From impl
-
 impl<'g, V: Clone, E: Clone, C: Cyclicness> Iterator for Dft<'g, V, E, C> {
     type Item = &'g Vertex<V>;
 
+    /// Gets the next item of the iterator.
     fn next(&mut self) -> Option<Self::Item> {
         if self.counter >= self.vertices.len() + 1 {
             return None;
         }
 
         if self.counter == 0 {
-            let first_vertex = self.edges[self.counter].vertices[0];
+            let first_vertex = self.edges[self.counter].vertex_indices[0];
             self.visited_vertices.insert(first_vertex);
 
             self.counter += 1;
             return Some(&self.vertices[first_vertex]);
         } else if self.counter == 1 {
-            let second_vertex = self.edges[self.counter - 1].vertices[1];
+            let second_vertex = self.edges[self.counter - 1].vertex_indices[1];
             self.visited_vertices.insert(second_vertex);
 
             self.counter += 1;
@@ -107,7 +116,7 @@ impl<'g, V: Clone, E: Clone, C: Cyclicness> Iterator for Dft<'g, V, E, C> {
         }
 
         let current_edge = &self.edges[self.counter - 1];
-        let destination_vertex = current_edge.vertices[1];
+        let destination_vertex = current_edge.vertex_indices[1];
 
         self.counter += 1;
         if !C::is_cyclic() {
@@ -120,5 +129,3 @@ impl<'g, V: Clone, E: Clone, C: Cyclicness> Iterator for Dft<'g, V, E, C> {
         Some(&self.vertices[destination_vertex])
     }
 }
-
-// TODO: implement iterator for Dft<stable_graph::StableGraph>*/
