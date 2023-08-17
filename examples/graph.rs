3e6e83413903235f641a0ddecd17101e0e6a42e3
AmJayden <cosbyemails@gmail.com>
2023-08-16 20:22:38 -0400

    shortens graph example

--- examples/graph.rs
+++ examples/graph.rs
@@ -3,39 +3,22 @@ use graffy::codegen::{formatter::*, *};
 fn main() {
     let mut formatter = Formatter::default();
     formatter.visit_statements(
-        &EdgeKindsBuilder::default()
-            .graph(
-                GraphBuilder::default().name("AttributeGraph").edges(
-                    EdgeKindsBuilder::default()
-                        .node(
-                            NodeKind::Node,
-                            Some(
-                                AttributesBuilder::default()
-                                    .shape(ShapeKind::Ellipse)
-                                    .color(ColorKind::Blue)
-                                    .into(),
-                            ),
-                        )
-                        .node(
-                            NodeKind::Custom("A".into()),
-                            Some(AttributesBuilder::default().label("Node A").into()),
-                        )
-                        .node(
-                            NodeKind::Custom("B".into()),
-                            Some(AttributesBuilder::default().label("Node B").into()),
-                        )
-                        .edge(
-                            EdgeBuilder::default()
-                                .left(NodeKind::Custom("A".into()))
-                                .right(NodeKind::Custom("B".into()))
-                                .attributes(AttributesBuilder::default().label("Edge from A to B")),
-                        )
-                        .build()
-                        .unwrap(),
-                ),
-            )
-            .build()
-            .unwrap(),
+        EdgeKindsBuilder::default().graph(
+            GraphKind::builder().name("H").edges(
+                EdgeKindsBuilder::default()
+                    .subgraph(
+                        SubGraph::builder().name("G").body(
+                            EdgeKindsBuilder::default()
+                                .node("C", AttributesBuilder::default().label("duude"))
+                                .node("D", AttributesBuilder::default().label("howww"))
+                                .edge(EdgeBuilder::default().left("C").right("D")),
+                        ),
+                    )
+                    .node("A", AttributesBuilder::default().label("IDK"))
+                    .node("B", AttributesBuilder::default().label("ANYMORE"))
+                    .edge(EdgeBuilder::default().left("A").right("B")),
+            ),
+        ),
     );
 
     println!("{}", formatter.source());
